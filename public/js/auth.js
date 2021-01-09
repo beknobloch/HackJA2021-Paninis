9af89abc3c15aa3ba59bec665108be913bd840b2
YoYoBoBo <46692722+YoYoBoBo@users.noreply.github.com>
2021-01-09 11:58:38 -0500

    attempt at fixing resume

--- public/js/auth.js
+++ public/js/auth.js
@@ -15,6 +15,7 @@ function convertToBase64() {
     //Read File
     let selectedFiles = document.getElementById("myFile").files;
     //Check File is not Empty
+    console.log(selectedFiles.length);
     if (selectedFiles.length > 0) {
         // Select the very first file from list
         let fileToLoad = selectedFiles[0];
@@ -58,7 +59,7 @@ try{
             pos = ele[i].value;
     } 
     
-    convertToBase64();
+    //convertToBase64();
     signupForm.addEventListener('submit', (e) => {
     // prevent refresh (losing info)
     e.preventDefault();
@@ -92,7 +93,7 @@ try{
 
     auth.onAuthStateChanged(user => {
         db.collection('users').doc(docID).update({
-            userID:  "none" || user.uid
+            userID:  user.uid || "none" 
         })
     })
 })
